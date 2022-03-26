2cf56a64b537770e45276b6852a49e28368bbde7
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-03-26 11:35:00 +0100

    Agrega hoja de estilo y javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("hola mundo");
+// window.onload = function () {
+//   document.getElementById("titulo").innerHTML="mi web";
+// }
+
+document.getElementById("titulo").innerHTML="mi web";
