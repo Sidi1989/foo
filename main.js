8b8e92e3d1c9ac2ef56d2f8bd9479067f6ff0ad3
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-03-31 12:52:41 +0200

    Eliminación de mains y Creación de carpetas para tipos de archivo

--- main.js
+++
@@ -1,6 +0,0 @@
-console.log("hola mundo");
-// window.onload = function () {
-//   document.getElementById("titulo").innerHTML="mi web";
-// }
-
-document.getElementById("titulo").innerHTML="mi web";
