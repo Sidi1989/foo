ec029d9e4a424c65668be1e1ae77a9e4071be213
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-04-06 18:58:08 +0200

    Limpiado app.js de códigos de ejemplo

--- server/app.js
+++ server/app.js
@@ -1,37 +1,29 @@
+const fs = require('fs');
 const express = require("express");
-const templates = require('./templates')
 const {bookTemplate} = require('./templates')
-//Lo de arriba es como si ae tuviera esto -> var bookTemplate = templates.bookTemplate
+
+
+
 
 const app = express();
 const port = process.argv[2] || 3000;
-var helloHandler = function (req, res) {
-  res.send("Hello");
-}
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('I am root')
 });
 
 
+var helloHandler = function (req, res) {
+  res.send("Hello");
+};
 app.get('/hello', helloHandler);
 
+
 var bookhandler = function (req, res) {
-  res.send(templates.bookTemplate);
-}
-
-
-
+  res.send(bookTemplate);
+};
 app.get('/books/:book', bookhandler);
-
-app.get('/foo', (req, res) => {
-  var saludo = `hola ${req.query.name}, `;
-  const city = 'estoy en madrid';
-  var mystring = saludo + city;
-  mystring += ' y hace frío';
-  res.send(mystring)
-});
 
 
 app.listen(port, () => {
