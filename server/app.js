d6b8e0b642d992a41ff83624a83187e08670cc6c
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-05-02 17:31:18 +0200

    Corrige rutas de ficheros estáticos

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const {renderFile} = require('ejs');
 const {bookformHandler, booksearchHandler, bookprofileHandler} = require('./controllers/books.js');
@@ -14,6 +15,15 @@ const port = process.argv[2] || 3004;
 
 app.engine('html', renderFile);
 
+app.use(function (req, res, next) {
+  console.log(req.originalUrl);
+  return next();
+});
+
+const options = {};
+const publicDirname = path.join(__dirname, '../Pinakes');
+app.use('/public', express.static(publicDirname, options));
+
 app.get('/books/new', bookformHandler);
 app.get('/books/search', booksearchHandler);
 app.get('/books/:book', bookprofileHandler);
@@ -23,11 +33,12 @@ app.get('/auth', userloginHandler);
 app.get('/auth/new', userformHandler);
 app.get('/users/:user', userprofileHandler);
 
-//Tarea: añadir criba de colecciones a collectionform
-//Tarea: crear los bucles de los géneros en bookform y petitionform
-//Tarea: problema en bookprofile con sede y reviews
+//Añadir criba de colecciones a collectionform
+//Crear los bucles de los géneros en bookform y petitionform
+//Problema en bookprofile con sede y reviews
+//Rutas public de header y footer, y problema con footer
 
 
-app.listen(port, () => {
+app.listen(port, function () {
   console.log(`Pinakes se escucha en el port ${port}`)
 })
