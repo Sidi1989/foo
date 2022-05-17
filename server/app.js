77e331034654d6bf716c5f815154beecf3107a91
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-05-17 12:00:49 +0200

    Instala cookie parser y realiza una prueba con ello

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const {renderFile} = require('ejs');
 const {bookformHandler, booksearchHandler, bookprofileHandler} = require('./controllers/books.js');
 const {collectionformHandler, collectionprofileHandler} = require('./controllers/collections.js');
@@ -17,14 +18,30 @@ const port = process.argv[2] || 3004;
 
 app.engine('html', renderFile);
 
+const options = {};
+const publicDirname = path.join(__dirname, '../Pinakes');
+app.use('/public', express.static(publicDirname, options));
+
 app.use(function (req, res, next) {
   console.log(req.originalUrl);
   return next();
 });
 
-const options = {};
-const publicDirname = path.join(__dirname, '../Pinakes');
-app.use('/public', express.static(publicDirname, options));
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  res.cookie('user', 'u1a');
+  console.log(req.cookies);
+  return next();
+});
+
+app.use(function (req, res, next) {
+  req.user = {};
+  req.user.id = req.cookies.user;
+  console.log(req.user);
+  return next();
+});
+
 
 app.get('/books/new', bookformHandler);
 app.get('/books/search', booksearchHandler);
