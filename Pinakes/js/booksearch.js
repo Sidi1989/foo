a51b90d0c0f7e3fd952d5838458ba2793692db35
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-05-19 11:53:14 +0200

    Cra botón de filtrado básico en bookSearch

--- Pinakes/js/booksearch.js
+++ Pinakes/js/booksearch.js
@@ -1,3 +1,5 @@
+var books = [];
+
 window.onload = function () {
   var bookSearchingInputNode = document.getElementById("bookSearchingInput");
   bookSearchingInputNode.onkeyup = function (event) {
@@ -5,14 +7,15 @@ window.onload = function () {
     var url = '/api/books';
     fetch(url)
       .then(response => response.json())
-      .then(function (books) {
+      .then(function (booksFromApi) {
           var searchTableBodyElement = document.getElementById('searchTableBody');
           searchTableBodyElement.innerHTML = '';
-          var filteredBooks = books.filter(function (book, i, a) {
+          var filteredBooks = booksFromApi.filter(function (book, i, a) {
             return book.title.toLowerCase().startsWith(userSearch.toLowerCase())
           });
-          filteredBooks.forEach(function (book, i) {
-            const newRowElement = document.createElement('tr')
+          books = filteredBooks;
+          books.forEach(function (book, i) {
+            const newRowElement = document.createElement('tr');
             newRowElement.innerHTML = `
                 <td>
                   <a class="text-primary" href="/books/${book.id}"
@@ -27,6 +30,31 @@ window.onload = function () {
             `;
             searchTableBodyElement.appendChild(newRowElement);
           });
+      });
+  };
+
+
+  var nuevoBotonNode = document.getElementById("nuevoBoton");
+  nuevoBotonNode.onclick = function (event) {
+      var searchTableBodyElement = document.getElementById('searchTableBody');
+      searchTableBodyElement.innerHTML = '';
+
+      var sortedBooks = books.slice(0,1);
+      sortedBooks.forEach(function (book, i) {
+        const newRowElement = document.createElement('tr')
+        newRowElement.innerHTML = `
+            <td>
+              <a class="text-primary" href="/books/${book.id}"
+                <p>${book.title}</p>
+              </a>
+            </td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td>${book.category}</td>
+            <td>${book.subcategory}</td>
+            <td>${book.language}</td>
+        `;
+        searchTableBodyElement.appendChild(newRowElement);
       });
   };
 };
