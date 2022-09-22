f8c05a42855cfdf6cf40525308cdfef58f8c6abf
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-09-22 16:39:13 +0200

    Cambia muy ligeramente el script que convierte las carpetas json en un solo jsonl

--- scripts/export-db.sh
+++ scripts/export-db.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Ejecutar siempre desde la carpeta Scripts
-echo "running"
+echo "beginning"
 cat ../runtime/db/authors/*.json | jq -c "." > ../runtime/db-export/authors.jsonl
 cat ../runtime/db/books/*.json | jq -c "." > ../runtime/db-export/books.jsonl
 cat ../runtime/db/categories/*.json | jq -c "." > ../runtime/db-export/categories.jsonl
@@ -12,4 +12,5 @@ cat ../runtime/db/members/*.json | jq -c "." > ../runtime/db-export/members.json
 cat ../runtime/db/petitions/*.json | jq -c "." > ../runtime/db-export/petitions.jsonl
 cat ../runtime/db/quotes/*.json | jq -c "." > ../runtime/db-export/quotes.jsonl
 cat ../runtime/db/reviews/*.json | jq -c "." > ../runtime/db-export/reviews.jsonl
-cat ../runtime/db/subacategories/*.json | jq -c "." > ../runtime/db-export/subcategories.jsonl
+cat ../runtime/db/subcategories/*.json | jq -c "." > ../runtime/db-export/subcategories.jsonl
+echo "ending"
