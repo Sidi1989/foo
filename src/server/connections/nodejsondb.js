c7774f808fb6e9c0b795fe9c4b48a177499d97ef
Miguel Diaz <miguel_diaz_laclaustra@hotmail.com>
2022-10-04 10:12:51 +0200

    Nuevas funciones con la sintaxis switch, y Cambia de true a false la configuración de guardado del JsonDB

--- src/server/connections/nodejsondb.js
+++ src/server/connections/nodejsondb.js
@@ -1,5 +1,6 @@
-const {JsonDB, Config} = require('node-json-db');
 const path = require('path');
+const _ = require('lodash');
+const {JsonDB, Config} = require('node-json-db');
 
 
 
@@ -12,8 +13,123 @@ var relativeDirname = '../../../runtime/db-migration';
 var absoluteDirname = path.join(__dirname, relativeDirname);
 var basename = 'PinakesDB.json';
 var pathname = path.join(absoluteDirname, basename);
-const configuration = new Config(pathname, true, false, '.');
-var db = new JsonDB(configuration);
+const configuration = new Config(pathname, false, false, '.');
+var jsonDb = new JsonDB(configuration);
+
+
+var read = async function (type) {
+  var relativeRoute;
+  switch (type) {
+    case 'author':
+      relativeRoute = '.authors';
+      break;
+    case 'book':
+      relativeRoute = '.books';
+      break;
+    case 'category':
+      relativeRoute = '.categories';
+      break;
+    case 'collection':
+      relativeRoute = '.collections';
+      break;
+    case 'language':
+      relativeRoute = '.languages';
+      break;
+    case 'location':
+      relativeRoute = '.locations';
+      break;
+    case 'member':
+      relativeRoute = '.members';
+      break;
+    case 'petition':
+      relativeRoute = '.petitions';
+      break;
+    case 'quote':
+      relativeRoute = '.quotes';
+      break;
+    case 'review':
+      relativeRoute = '.reviews';
+      break;
+    case 'subcategory':
+      relativeRoute = '.subcategories';
+      break;
+    default:
+      throw new Error(`type ${type} not recognized`);
+  }
+  const elements = await jsonDb.getData(relativeRoute);
+  return _.cloneDeep(elements);
+};
+
+
+var write = async function (type, id, info) {
+  var relativeRoute;
+  switch (type) {
+    case 'book':
+      relativeRoute = '.books';
+      break;
+    case 'collection':
+      relativeRoute = '.collections';
+      break;
+    case 'location':
+      relativeRoute = '.locations';
+      break;
+    case 'member':
+      relativeRoute = '.members';
+      break;
+    case 'petition':
+      relativeRoute = '.petitions';
+      break;
+    case 'review':
+      relativeRoute = '.reviews';
+      break;
+    default:
+      throw new Error(`type ${type} not recognized`);
+  }
+  const elements = await jsonDb.push(relativeRoute, id, 'info que escribe');
+  return _.cloneDeep(elements);
+};
+
+
+var erase = async function (type, id) {
+  var relativeRoute;
+  switch (type) {
+    case 'book':
+      relativeRoute = '.books';
+      break;
+    case 'collection':
+      relativeRoute = '.collections';
+      break;
+    case 'location':
+      relativeRoute = '.locations';
+      break;
+    case 'member':
+      relativeRoute = '.members';
+      break;
+    case 'petition':
+      relativeRoute = '.petitions';
+      break;
+    case 'review':
+      relativeRoute = '.reviews';
+      break;
+    default:
+      throw new Error(`type ${type} not recognized`);
+  }
+  const elements = await jsonDb.delete(relativeRoute, id);
+  return _.cloneDeep(elements);
+};
+
+
+const db = {
+  read: read,
+  write: write,
+  erase: erase,
+
+  getData: jsonDb.getData.bind(jsonDb),
+  push: jsonDb.push.bind(jsonDb),
+  del: jsonDb.delete.bind(jsonDb),
+  save: jsonDb.save.bind(jsonDb),
+  reload: jsonDb.reload.bind(jsonDb)
+};
 
 
 
